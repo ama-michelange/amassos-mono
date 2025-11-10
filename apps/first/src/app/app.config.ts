7c5f6f09213ca9e9cf30f5dfd32b122d4e58bb09
ama.michelange <ama-michelange@users.noreply.github.com>
2025-11-10 22:49:23 +0100

    ci(project): Format first workflow

--- apps/first/src/app/app.config.ts
+++ apps/first/src/app/app.config.ts
@@ -2,9 +2,9 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
-} from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+} from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { appRoutes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
